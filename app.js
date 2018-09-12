82320a8c6f72e0637146ab79263ff7872805068a
ysb06 <ysb06@hotmail.com>
2018-09-12 17:10:14 +0900

    Basic function
    add an basic server code

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ const clova = require('./clova');
 
 const app = express();
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on ${SERVER_PORT} port`);
 });
+
+app.get('/', clova.clovaFulfillment);
+app.post('/', clova.clovaFulfillment);
--- clova/index.js
+++ clova/index.js
@@ -1,1 +1,76 @@
-console.log("Hello");
+exports.clovaFulfillment = function (req, res) {
+	let timeUtil = require('date-utils');
+	let cDate = new Date();
+	
+	let attributes = {
+		"formerIntent": 'NaN',
+		"recommendation": 0,
+		"recipe": "NaN",
+		"step": 0
+	}
+	let params = req.body;
+	
+	try {
+		let formerAttributes = params.session.sessionAttributes;
+		
+		attributes.recommendation = formerAttributes.recommendation;
+		attributes.recipe = formerAttributes.recipe;
+		attributes.step = formerAttributes.step;
+	} catch (e) {
+		console.log(e);
+	}
+	
+	try {
+		let intent = params.request.intent.name;
+		
+		attributes.formerIntent = intent;
+	} catch (e) {
+		console.log(e);
+	}
+	
+	
+	console.log('Time: ' + cDate.toFormat('YYYY-MM-DD HH24:MI:SS'));	
+	console.log('Intent: ' + attributes.formerIntent);	
+	
+	result = initializeJSON(attributes);
+	result.response.outputSpeech.values.push(getSpeech('테스트 중입니다.'));
+	result.response.outputSpeech.values.push(getURL('테스트 중입니다.'));
+	
+	res.json(result);
+};
+
+function initializeJSON(attributes) {
+	let result = {
+		"version": "0.1.0",
+		"sessionAttributes": attributes,
+		"response": {
+			"outputSpeech": {
+				"type": "SpeechList",
+				"values": []
+			},
+			"card": {},
+			"directives": [],
+			"shouldEndSession": false
+		}
+	};
+	
+	return result;
+}
+
+
+function getSpeech(text) {
+	return {
+        type: 'PlainText',
+        lang: 'ko',
+        value: text,
+	};
+}
+
+function getURL(url) {
+	return {
+        type: 'URL',
+        lang: '',
+        value: 'https://www.sample-videos.com/audio/mp3/crowd-cheering.mp3',
+	};
+}
+
