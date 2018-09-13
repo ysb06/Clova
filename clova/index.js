a4b73bae036fe9dc69830b5a88c1b5f4d63d7648
ysb06 <ysb06@hotmail.com>
2018-09-13 17:42:42 +0900

    Apply class
    Rebuild

--- clova/index.js
+++ clova/index.js
@@ -3,66 +3,128 @@ const uuid = require('uuid').v4;
 const timeUtil = require('date-utils');
 const { DOMAIN } = require('../config');
 
+class ClovaResult {
+	constructor (request) {
+		let attributes = {
+			"formerIntent": 'NaN',
+			"recommendation": 0,
+			"recipe": "NaN",
+			"step": 0
+		}
+		// Attribute 초기화, Attribute가 없으면 에러로 인한 무시
+		try {
+			attributes.recommendation = request.body.session.sessionAttributes.recommendation;
+			attributes.recipe = request.body.session.sessionAttributes.recipe;
+			attributes.step = request.body.session.sessionAttributes.step;
+		} catch (e) {
+			console.log(e);
+		}
+		// Result 속성 정의
+		this.result = {
+			"version": "0.1.0",
+			"sessionAttributes": attributes,
+			"response": {
+				"outputSpeech": {
+					"type": "SpeechList",
+					"values": []
+				},
+				"card": {},
+				"directives": [],
+				"shouldEndSession": false
+			}
+		}
+	}
+	
+	addSimpleSpeech(text) {
+		let plainText = {
+			type: 'PlainText',
+			lang: 'ko',
+			value: text,
+		};
+		
+		this.result.response.outputSpeech.values.push(plainText);
+	}
+	
+	addPlayDirective(url) {
+		let directive = {
+			header: {
+				namespace: "AudioPlayer",
+				name: "Play"
+			},
+			payload: {
+				audioItem: {
+					audioItemId: uuid(),
+					episodeId: 22346122,
+					stream: {
+						beginAtInMilliseconds: 0,
+						episodeId: 22346122,
+						playType: "NONE",
+						token: uuid(),
+						url: url,
+						urlPlayable: true
+					},
+					titleSubText1: 'Yori',
+					titleText: 'Waiting...',
+					type: 'custom'
+				},
+				playBehavior: "REPLACE_ALL",
+				source: {
+					name: "모두의 요리사"
+				}
+			}
+		};
+		
+		this.result.response.directives.push(directive);
+	}
+}
+
 exports.clovaFulfillment = function (req, res) {
 	let cDate = new Date();
-	let attributes = {
-		"formerIntent": 'NaN',
-		"recommendation": 0,
-		"recipe": "NaN",
-		"step": 0
-	}
+	console.log('Time: ' + cDate.toFormat('YYYY-MM-DD HH24:MI:SS'));
 	let params = req.body;
-	console.log('Request in -->');
-	console.log(params);
+	console.log('Request in -->\n' + params);
+	
+	let clovaReq = 'NaN';
 	
 	try {
-		let formerAttributes = params.session.sessionAttributes;
-		
-		attributes.recommendation = formerAttributes.recommendation;
-		attributes.recipe = formerAttributes.recipe;
-		attributes.step = formerAttributes.step;
+		clovaReq = params.request.type;
 	} catch (e) {
 		console.log(e);
 	}
 	
-	try {
-		let request = params.request.type;
-		switch(request) {
-			case 'LaunchRequest':
-				break;
-			case 'IntentRequest':
-				attributes.formerIntent = params.request.intent.name;
-				break;
-			case 'SessionEndedRequest':
-				result.response.outputSpeech.values.push(getSpeech('모두의 요리사 종료합니다'));
-				result.response.shouldEndSession = true;
-				break;
-		}
-		console.log('\nRequest: ' + request);
-	} catch (e) {
-		console.log(e);
-	}
-	
-	//intent request와 event Request는 다르게 처리해야 함
-	
-	console.log('Time: ' + cDate.toFormat('YYYY-MM-DD HH24:MI:SS'));	
-	console.log('Intent: ' + attributes.formerIntent + '\n');	
-	
-	let result = initializeJSON(attributes);
+	let clovaResponse = new ClovaResult(req);
 	//----------------- intent 및 event 처리 -------------------//
-	
-	result.response.outputSpeech.values.push(getSpeech('테스트 중입니다.'));
-	//result.response.outputSpeech.values.push(getURL(waitingMusic));
-	result.response.directives.push(getPlayDirective(waitingMusic));
-	
+	console.log('\n\n');
+	switch(clovaReq) {
+		case 'LaunchRequest':
+			clovaResponse.addSimpleSpeech('아무 말이나 해보세요.');
+			break;
+		case 'EventRequest':
+			let clovaEvent = params.request.event.name;
+			console.log('Event --> ' + clovaEvent);
+			clovaResponse.addSimpleSpeech('아직 뭘 해야 할지 모르겠네요.');
+			break;
+		case 'IntentRequest':
+			let intent = params.request.intent.name;
+			clovaResponse.addSimpleSpeech(intent + ' 인텐트. 샘플 노래를 재생합니다.');
+			clovaResponse.addPlayDirective(waitingMusic);
+			break;
+		default:
+			clovaResponse.addSimpleSpeech('에러가 발생했습니다.');
+			console.log('Request --> ' + clovaReq);
+			break;
+	}	
 	//----처리 완료----//
+
 	console.log('Response out -->');
-	console.log(result);
+	console.log(clovaResponse.result);
+	/*
 	console.log(result.response.outputSpeech.values);
 	console.log('<-- Directives -->');
 	console.log(result.response.directives);
 	console.log(result.response.directives[0].payload);
-	res.json(result);
+	*/
+	res.json(clovaResponse.result);
 };
 
 function initializeJSON(attributes) {
